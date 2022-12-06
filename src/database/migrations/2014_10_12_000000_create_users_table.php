de42f8da6f097269ff224e753dbdf0194140c791
aliJo <a.jomehri@viratarh.ir>
2022-12-06 17:55:55 +0330

    user model -m

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,15 +21,10 @@ class CreateUsersTable extends BaseMigration
      */
     protected function createTable(Blueprint $table): void
     {
-        $table->string(User::COLUMN_NAME, 100)
-            ->nullable(false);
-        $table->string(User::COLUMN_EMAIL, 150)
-            ->nullable(false)
-            ->unique();
-        $table->dateTime(User::COLUMN_EMAIL_VERIFIED_AT)
-            ->nullable();
-        $table->string(User::COLUMN_PASSWORD, 50)
-            ->nullable(false);
+        $table->string(User::COLUMN_NAME, 100)->nullable(false);
+        $table->string(User::COLUMN_EMAIL, 150)->nullable(false)->unique();
+        $table->dateTime(User::COLUMN_EMAIL_VERIFIED_AT)->nullable();
+        $table->string(User::COLUMN_PASSWORD, 50)->nullable(false);
         $table->string('remember_token', 100)->nullable();
     }
 
